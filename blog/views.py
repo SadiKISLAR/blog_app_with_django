16f19f75ca0edb4f2843c6e9654f12bafb46b379
SadiKISLAR <sadi22kislar@gmail.com>
2023-02-21 16:16:52 +0300

    secret key debug

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Blog
+from .forms import BlogForm
 
 # Create your views here.
+def home(request):
+    blogs = Blog.objects.all()
+    form = BlogForm()
+    context = {
+        'blogs' : blogs,
+        'form' : form
+    }
+    return render(request, context)
