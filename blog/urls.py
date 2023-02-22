8b074281d4f29fbda0b8e905db19c5f2656fcfaa
SadiKISLAR <sadi22kislar@gmail.com>
2023-02-22 10:17:49 +0300

    django restframe work added

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import home
+from rest_framework import routers
 
+router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('', home, name='add')
+    path('', include(router.urls))
 ]
--- main/settings.py
+++ main/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     
     #my apps
     'blog',
+    
+    #third party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
