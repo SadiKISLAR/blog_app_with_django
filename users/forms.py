56733146283107e954f2b7ef2d7af3d9c0764d0b
SadiKISLAR <sadi22kislar@gmail.com>
2023-02-23 18:42:22 +0300

    user forms created

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from dataclasses import fields
+from django import forms
+from .models import UserProfile
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('profile_pic', 'bio')
+        
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email')
