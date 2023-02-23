50f6eb6fbb65d570e7d64f36fc93c7a82d95cbd0
SadiKISLAR <sadi22kislar@gmail.com>
2023-02-23 19:33:45 +0300

    logout views url

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='user_login'),
-    path('logiouts/', register, name='user_login'),
+    path('logouts/', LogoutView.as_view(), name='logout_app'),
 ]
