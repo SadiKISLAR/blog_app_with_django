5fd5fb5327f156d6c7394a1d431e15728bb8a055
SadiKISLAR <sadi22kislar@gmail.com>
2023-02-23 17:52:08 +0300

    registe fınction created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+from users.forms import UserProfileForm, UseForm
+
 
 # Create your views here.
+
+
+def register(request):
+    form_user = UseForm()
+    form_profile = UserProfileForm()
+    if request.method == 'POST':
+        form_user = UseForm(request.POST, request.FILES)
+        if form_user.is_valid() and form_profile.is_valid():
+            user = form_user.save()
+            profile = form_profile.save(commit=F
+            profile.user = user
+            profile.save()
+            
+            login(request, user)
+            
+            return redirect('home')
+            
+    context = {
+        'form_user' : form_user,
+        'form_profile' : form_profile,
+        
+    }
+    
+    return render(request, context
+
+    
